d5d28e2fc39a1f8b054087a6445165632a17ccfe
Jeffrey Hicks <jhicks@cteh.com>
2009-06-23 23:58:05 -0500

    fix spacing/tabs in scaffold model

--- static_scaffold/templates/model.rb
+++ static_scaffold/templates/model.rb
@@ -1,29 +1,38 @@
 class <%= class_name %> < ActiveRecord::Base
-def self.table_name() "<%=gen_spec.table_name%>" end
 
-<%if gen_spec.ascendant -%>
-    def ascendant
-        <%=gen_spec.singular_name%>  
-    end
+<%if gen_spec.belongs_to -%>
+<%for bt in gen_spec.belongs_to.sort {|bt|bt["name"]} -%>
+  belongs_to <%=bt[:name].inspect%>, :class_name=><%=bt[:model].inspect%>, :foreign_key=><%=bt[:key].inspect%>
 <%end -%>
 
+<%end -%>
+  def self.table_name() 
+    "<%=gen_spec.table_name%>" 
+  end
+
+<%if gen_spec.ascendant -%>
+  def ascendant
+    <%=gen_spec.singular_name%>  
+  end
+
+<%end -%>
 <%if gen_spec.authorization_method.to_s == 'static_authorization' -%>
-    def permits?(user)
-        if user.has_global_permit_for(<%=gen_spec.plural_name.inspect%>)
-            return true
-        else
+  def permits?(user)
+    if user.has_global_permit_for(<%=gen_spec.plural_name.inspect%>)
+      return true
+    else
 <%if gen_spec.ascendant -%>
-            return (permitted_include?(user) and ascendant.permits?(user))
+      return (permitted_include?(user) and ascendant.permits?(user))
 <%else -%>
-            return permitted_include?(user)
+      return permitted_include?(user)
 <%end -%>
-        end
     end
+  end
 
-    def permitted_include?(user)
-        # Asside from ascendant constraints, is this user permitted to access this <%=gen_spec.singular_name%>
-        return true
-    end
+  def permitted_include?(user)
+    # Asside from ascendant constraints, is this user permitted to access this <%=gen_spec.singular_name%>
+    return true
+  end
 
 <%end -%>
 end
