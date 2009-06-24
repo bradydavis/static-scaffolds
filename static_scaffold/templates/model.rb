ed4f2cb6cd7d6972e8924c66b0b1cb6b41ccd136
Jeffrey Hicks <jhicks@cteh.com>
2009-06-23 20:50:05 -0500

    introduce language in generated code to distinguish different type of authorization 'scoped', 'global'

--- static_scaffold/templates/model.rb
+++ static_scaffold/templates/model.rb
@@ -2,15 +2,17 @@ class <%= class_name %> < ActiveRecord::Base
 def self.table_name() "<%=gen_spec.table_name%>" end
 
 <%if gen_spec.authorize_method== :static_authorization -%>
-    def authorize(user)
+    def authorize_scope(user)
 <%if gen_spec.parent -%>
-        user.authorize_any(<%=gen_spec.singular.inspect%>) or (<%=gen_spec.singular_name%>_authorize(user) and <%=gen_spec.parent["name"]%>.authorize(user))
+        user.authorize_global_access(<%=gen_spec.singular.inspect%>) 
+        or (<%=gen_spec.singular_name%>_authorize_scope(user) and <%=gen_spec.parent["name"]%>.authorize_scope(user))
 <%else -%>
-        user.authorize_any(<%=gen_spec.singular.inspect%>) or <%=gen_spec.singular_name%>_authorize(user)
+        user.authorize_global_access(<%=gen_spec.singular.inspect%>) or <%=gen_spec.singular_name%>_authorize_scope(user)
 <%end -%>
     end
 
-    def <%=gen_spec.singular_name%>_authorize
+    def <%=gen_spec.singular_name%>_authorize_scope(user)
+        # Impliment custom scoping rules here
         return true
     end
 <%end -%>
