751729a5f1745ebecd1db829fc77d08aa6d7ebca
Jeffrey Hicks <jhicks@cteh.com>
2009-06-24 23:50:52 -0500

    flesh out beginnings of layout generator, get enough working to yield the title in the generated views

--- static_scaffold/static_scaffold_generator.rb
+++ static_scaffold/static_scaffold_generator.rb
@@ -69,18 +69,18 @@ class StaticScaffoldGenerator < Rails::Generator::NamedBase
       end
 
       # Layout and stylesheet.
-      #m.template('layout.html.erb', File.join('app/views/layouts', controller_class_path, "#{controller_file_name}.html.erb"))
-      #m.template('style.css', 'public/stylesheets/scaffold.css')
+      m.template('layout.html.erb', File.join('app/views/layouts', controller_class_path, "#{controller_file_name}.html.erb"))
+      m.template('style.css', 'public/stylesheets/scaffold.css')
 
-      #m.template(
-      #  'controller.rb', File.join('app/controllers', controller_class_path, "#{controller_file_name}_controller.rb")
-      #)
+      m.template(
+        'controller.rb', File.join('app/controllers', controller_class_path, "#{controller_file_name}_controller.rb")
+      )
 
-      #m.template('functional_test.rb', File.join('test/functional', controller_class_path, "#{controller_file_name}_controller_test.rb"))
-      #m.template('helper.rb',          File.join('app/helpers',     controller_class_path, "#{controller_file_name}_helper.rb"))
-      #m.template('helper_test.rb',     File.join('test/unit/helpers',    controller_class_path, "#{controller_file_name}_helper_test.rb"))
+      m.template('functional_test.rb', File.join('test/functional', controller_class_path, "#{controller_file_name}_controller_test.rb"))
+      m.template('helper.rb',          File.join('app/helpers',     controller_class_path, "#{controller_file_name}_helper.rb"))
+      m.template('helper_test.rb',     File.join('test/unit/helpers',    controller_class_path, "#{controller_file_name}_helper_test.rb"))
 
-      #m.route_resources controller_file_name
+      m.route_resources controller_file_name
 
       #m.dependency 'model', [name] + @args, :collision => :skip
     end
