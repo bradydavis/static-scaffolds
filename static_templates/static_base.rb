bb9e0a2b68548e0d4633b1627344ae764eb96685
Jeffrey Hicks <jhicks@cteh.com>
2009-06-25 10:30:06 -0500

    added required plugin install to template, TODO: apply patch for fix inflector per http://groups.google.com/group/tog_users/browse_thread/thread/a94e0cd456d8ea12

--- static_templates/static_base.rb
+++ static_templates/static_base.rb
@@ -1,3 +1,6 @@
 gem 'jrhicks-static-scaffold'
  
 rake("gems:install", :sudo => true)
+
+run "sudo ruby script/plugin install http://opensvn.csie.org/rails_file_column/plugins/file_column/trunk"
+run "patch line 619 "
