46560c970bd05af784f7323737bb7a5374fb758a
Jeffrey Hicks <jhicks@cteh.com>
2009-06-25 20:27:09 -0500

    add template action to patch* file_column.rb to gix 'Inflector' bug that made file_column not compatible with Rails 2.3

--- static_templates/static_base.rb
+++ static_templates/static_base.rb
@@ -2,5 +2,7 @@ gem 'jrhicks-static-scaffold'
  
 rake("gems:install", :sudo => true)
 
+# Install file column plugin and overwrite file_column.rb that has bug in Rails 2.3
 run "sudo ruby script/plugin install http://opensvn.csie.org/rails_file_column/plugins/file_column/trunk"
-run "patch line 619 "
+run "curl http://gist.github.com/raw/136263/8a130be758d9a904ccfb8cf20e78643087bdf98e/file_column.rb > #{File.join(RAILS_ROOT,"vendor","plugins","file_column","lib","file_column.rb")}"
+
