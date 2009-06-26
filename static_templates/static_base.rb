fd8973ca1b1f224320c4907543d277b05d3e1609
Jeffrey Hicks <jhicks@cteh.com>
2009-06-25 22:16:22 -0500

    commented out .irbrc thing that doesn't belong in a rails template (although its cool)

--- static_templates/static_base.rb
+++ static_templates/static_base.rb
@@ -6,3 +6,8 @@ rake("gems:install", :sudo => true)
 run "sudo ruby script/plugin install http://opensvn.csie.org/rails_file_column/plugins/file_column/trunk"
 run "curl http://gist.github.com/raw/136263/8a130be758d9a904ccfb8cf20e78643087bdf98e/file_column.rb > #{File.join(RAILS_ROOT,"vendor","plugins","file_column","lib","file_column.rb")}"
 
+# This is probably rude to do this here
+# overwrite_irbrc = ask("Do you want to overwrite your personal .irbrc to customize your irb? (y/n)")
+# if overwrite_irbrc=="y"
+#  run "curl http://gist.github.com/raw/136255/c1ed51b0bae9710d03fa52d8a509e1f3a1c0f8a5/.irbrc > ~/.irbrc"
+#end
