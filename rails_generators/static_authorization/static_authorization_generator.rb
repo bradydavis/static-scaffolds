edc9bc47a8b5e49fe49a393b134c202d776ee868
Jeffrey Hicks <jhicks@cteh.com>
2009-07-06 14:22:21 -0500

    ensure migrate folder exists before generating migrations

--- rails_generators/static_authorization/static_authorization_generator.rb
+++ rails_generators/static_authorization/static_authorization_generator.rb
@@ -3,6 +3,7 @@ class StaticAuthorizationGenerator <  Rails::Generator::Base
     record do |m| 
 
         # Migration
+        m.directory(File.join('db','migrate'))        
         m.template(
               "migration.rb",
               File.join('db','migrate',"#{Time.now.strftime("%Y%m%d%H%M%S")}_static_authorization.rb"))
