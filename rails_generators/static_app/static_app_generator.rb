32cb1acb07ba4ae96d4bec5e91b2424415edb719
Jeffrey Hicks <jhicks@cteh.com>
2009-07-08 00:35:44 -0500

    Integrate Blueprint CSS into layout.  Organize static_app generator

--- rails_generators/static_app/static_app_generator.rb
+++ rails_generators/static_app/static_app_generator.rb
@@ -11,15 +11,36 @@ class StaticAppGenerator < Rails::Generator::Base
       m.directory 'app/helpers'
       m.template "layout.html.erb", "app/views/layouts/application.html.erb"
       m.file "static_style.css", "public/stylesheets/static_style.css"
-      m.file "sorttable.js", "public/javascripts/sorttable.js"
       m.file "application_helper.rb", "app/helpers/application_helper.rb"      
       m.file "layout_helper.rb", "app/helpers/layout_helper.rb"
+
+      # BluePrint CSS
+      m.file "blueprint/grid.png", "public/images/blueprint_css_grid.png"
+      m.file "blueprint/print.css", "public/stylesheets/print.css"
+      m.file "blueprint/ie.css", "public/stylesheets/ie.css"
+      m.file "blueprint/screen.css", "public/stylesheets/screen.css"
+
+      # header and footer
+      m.directory 'app/views/shared'
+      m.template "standard_navigation.html.erb", "app/views/shared/_standard_navigation.html.erb"
+      m.template "standard_header.html.erb", "app/views/shared/_standard_header.html.erb"
+      m.template "standard_footer.html.erb", "app/views/shared/_standard_footer.html.erb"
+
+      # Sorttable Tables
+      m.file "sorttable/sorttable.js", "public/javascripts/sorttable.js"  
+                  
+      # icons
+      m.directory 'public/images/icons'
+      pngs = "tick cross feed key doc external pdf email visited xls".split()
+      for file in pngs
+        m.file "icons/#{file}.png", "public/images/icons/#{file}.png"
+      end
       
       #Uni-formn
-      m.file "uni-form-generic.css", "public/stylesheets/uni-form-generic.css"
-      m.file "uni-form.css","public/stylesheets/uni-form.css"
-      m.file "uni-form.jquery.js", "public/javascripts/uni-form.jquery.js"
-      m.file "jquery.js", "public/javascripts/jquery.js"      
+      m.file "uni-form/uni-form-generic.css", "public/stylesheets/uni-form-generic.css"
+      m.file "uni-form/uni-form.css","public/stylesheets/uni-form.css"
+      m.file "uni-form/uni-form.jquery.js", "public/javascripts/uni-form.jquery.js"
+      m.file "uni-form/jquery.js", "public/javascripts/jquery.js"      
     end
   end
    
