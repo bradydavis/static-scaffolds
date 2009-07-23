348ea2f2f8e814ada9180ac301944c8d8622d577
Jeffrey Hicks <jhicks@cteh.com>
2009-07-22 21:32:58 -0500

    Work on the global navigation a bit ... starting to rethink local navigation / nested resource navigation ... hmm

--- rails_generators/static_app/static_app_generator.rb
+++ rails_generators/static_app/static_app_generator.rb
@@ -91,10 +91,10 @@ class StaticAppGenerator < Rails::Generator::Base
 
       # header and footer
       m.directory 'app/views/shared'
-      m.template "standard_navigation.html.erb", "app/views/shared/_standard_navigation.html.erb"
       m.template "standard_header.html.erb", "app/views/shared/_standard_header.html.erb"
       m.template "standard_footer.html.erb", "app/views/shared/_standard_footer.html.erb"
       m.template "standard_banner.html.erb", "app/views/shared/_standard_banner.html.erb"
+      m.template "root_navigation.html.erb", "app/views/shared/_root_navigation.html.erb"
       m.file "header_bg.png", "public/images/header_bg.png"
       #m.template "standard_banner_user.html.erb", "app/views/shared/_standard_banner_user.html.erb"
 
